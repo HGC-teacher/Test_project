90f9d528d32beda198f0da12816e3256418d829e
HGC-teacher <kmccoll@hazelglencollege.com>
2021-06-09 14:49:00 +1000

    mashup

--- stuff.py
+++ stuff.py
@@ -2,4 +2,8 @@ for i in range(10):
     print(i)
 print("Counting is fun!")
 
+<<<<<<< Updated upstream
 print("will this die?");
+=======
+print([x for x in 'jshdjsfjhsgfjhsdf'])
+>>>>>>> Stashed changes
